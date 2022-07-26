6f6f9607ec7f53ee3f698d149e9fcea16201aea5
Alexey-Lysenko <63449694+Alexey-Lysenko@users.noreply.github.com>
2022-07-26 16:38:13 +0700

    added toast

--- app/src/main/java/com/lesha/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/lesha/criminalintent/CrimeListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -48,14 +49,21 @@ class CrimeListFragment: Fragment() {
         crimeRecyclerView.adapter = adapter
     }
 
-    private inner class CrimeHolder(binding: ListItemCrimeBinding): RecyclerView.ViewHolder(binding.root){
+    private inner class CrimeHolder(binding: ListItemCrimeBinding): RecyclerView.ViewHolder(binding.root),View.OnClickListener{
         private lateinit var crime: Crime
         val titleTextView: TextView = itemView.findViewById(binding.crimeTitle.id)
         val dateTextView: TextView = itemView.findViewById(binding.crimeDate.id)
+        init {
+            itemView.setOnClickListener(this)
+        }
         fun bind(crime: Crime){
             this.crime = crime
             titleTextView.text = this.crime.title
             dateTextView.text = this.crime.date.toString()
+        }
+
+        override fun onClick(p0: View?) {
+            Toast.makeText(context,"${crime.title}!",Toast.LENGTH_SHORT).show()
         }
     }
 
