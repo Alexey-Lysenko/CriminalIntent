0e769eb33d906312cd4a875d5466d9be2db409a4
Alexey-Lysenko <63449694+Alexey-Lysenko@users.noreply.github.com>
2022-08-17 15:01:30 +0700

    *linked CrimeFragment.kt with CrimeDetailViewModel.kt *override onCreate function for initialize crime field and get crimeId specific instance crime *override onViewCreated function for subscribe crimeLiveData field of CrimeDetailViewModel *override onStop function where we saving all changes to the instance *added updateUI function *added newInstance function where pass int argument ID of crime, and then save it in bundle.

--- app/src/main/java/com/lesha/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/lesha/criminalintent/CrimeFragment.kt
@@ -11,8 +11,11 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.lesha.criminalintent.databinding.FragmentCrimeBinding
 import java.text.SimpleDateFormat
+import java.util.*
 
 class CrimeFragment : Fragment() {
 
@@ -20,6 +23,15 @@ class CrimeFragment : Fragment() {
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
     private lateinit var solvedCheckBox: CheckBox
+
+    private val crimeDetailViewModel: CrimeDetailViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+        val crimeId: UUID = arguments?.getSerializable(ARG_CRIME_ID) as UUID
+        crimeDetailViewModel.loadCrime(crimeId)
+    }
 
     @SuppressLint("SimpleDateFormat")
     override fun onCreateView(
@@ -30,11 +42,24 @@ class CrimeFragment : Fragment() {
         val binding = FragmentCrimeBinding.inflate(inflater, container, false)
         titleField = binding.crimeTitle
         dateButton = binding.crimeDate.apply {
-            text = SimpleDateFormat(DateConstants.FORMAT_PATTERN).format(crime.date)
+            text = SimpleDateFormat(DateConstants.FORMAT_PATTERN).format(binding.crimeDate.id)
             isEnabled = false
         }
         solvedCheckBox = binding.crimeSolved
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeDetailViewModel.crimeLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crime ->
+                crime?.let {
+                    this.crime = crime
+                    updateUI()
+                }
+            }
+        )
     }
 
     override fun onStart() {
@@ -52,6 +77,32 @@ class CrimeFragment : Fragment() {
 
         solvedCheckBox.setOnCheckedChangeListener { _, isChecked ->
             crime.isSolved = isChecked
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        crimeDetailViewModel.saveCrime(crime)
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun updateUI() {
+        titleField.setText(crime.title)
+        dateButton.text = SimpleDateFormat(DateConstants.FORMAT_PATTERN).format(crime.date)
+        solvedCheckBox.apply{
+            isChecked = crime.isSolved
+            jumpDrawablesToCurrentState()
+        }
+    }
+
+    companion object {
+        const val ARG_CRIME_ID = "crime_id"
+
+        fun newInstance(crimeId: UUID): CrimeFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_CRIME_ID, crimeId)
+            }
+            return CrimeFragment().apply { arguments = args }
         }
     }
 
